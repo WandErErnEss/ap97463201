a940475b1f979f3a15eba1a67c30579b2e560955
WandErErnEss <44531592+WandErErnEss@users.noreply.github.com>
2019-03-20 15:49:37 +0330

    Add files via upload

---
+++ assignment2/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String args[])
+    {
+        int i,j,n;
+        System.out.print("enter n:");
+        Scanner a=new Scanner(System.in);
+        n=a.nextInt();
+        n=n-(n/2);
+        for(i=1;i<=n;i++)
+        {
+            for(j=i;j<n;j++)
+            {
+                System.out.print(" ");
+            }
+            for(j=1;j<=(2*i-1);j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for(i=n;i>=1;i--)
+        {
+            for(j=i;j<=n;j++)
+            {
+                System.out.print(" ");
+            }
+            for(j=2;j<(2*i-1);j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
